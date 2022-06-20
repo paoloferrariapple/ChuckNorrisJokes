112e35f66252ea422e85ee0c3c3986ed048474fd
Paolo ferrari <paoloferrariapple@gmail.com>
2022-06-20 12:15:55 +0200

    Initial Commit

---
+++ FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwiftUIApp.swift
+//  FirstAppSwiftUI
+//
+//  Created by user on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
